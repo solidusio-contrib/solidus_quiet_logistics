7420c88dd6e5200511ca34fcb9c91a0c8666c3be
Andrea Vassallo <andrea.vassallo.94@gmail.com>
2019-01-28 13:16:26 +0100

    Add some configuration attributes
    Expose the gem settings for the connection to QL and AWS.

--- lib/solidus_quiet_logistics.rb
+++ lib/solidus_quiet_logistics.rb
@@ -1,2 +1,21 @@
 require 'solidus_core'
 require 'solidus_quiet_logistics/engine'
+
+class Configuration
+  # QL
+  attr_accessor :enabled, :client_id, :business_unit, :warehouse, :order_special_service_amount
+
+  # AWS
+  attr_accessor :aws_region, :aws_access_key_id, :aws_secret_access_key,
+    :aws_inbox_queue_url, :aws_inbox_bucket, :aws_outbox_bucket, :aws_outbox_queue_url
+end
+
+module SolidusQuietLogistics
+  def self.configuration
+    @configuration ||= Configuration.new
+  end
+
+  def self.configure
+    yield configuration
+  end
+end
