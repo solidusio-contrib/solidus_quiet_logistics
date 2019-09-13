f71c8111cd9b7496cf223bf7ae521bb205db5296
Alessandro Desantis <desa.alessandro@gmail.com>
2019-09-13 09:25:30 +0200

    Fix RMA acronym in migration names
    The RMA acronym is added to the Rails inflector by solidus_core,
    so we must capitalize RMA as an acronym too in order for the Rails
    autoloader to work.

--- db/migrate/20190612082744_add_ql_rma_sent_timestamp_to_spree_inventory_unit.rb
+++ db/migrate/20190612082744_add_ql_rma_sent_timestamp_to_spree_inventory_unit.rb
@@ -1,4 +1,4 @@
-class AddQlRmaSentTimestampToSpreeInventoryUnit < ActiveRecord::Migration[5.2]
+class AddQlRMASentTimestampToSpreeInventoryUnit < ActiveRecord::Migration[5.2]
   def change
     add_column :spree_inventory_units, :ql_rma_sent, :datetime
   end
--- db/migrate/20190625073424_add_ql_rma_received_date_to_spree_inventory_unit.rb
+++ db/migrate/20190625073424_add_ql_rma_received_date_to_spree_inventory_unit.rb
@@ -1,4 +1,4 @@
-class AddQlRmaReceivedDateToSpreeInventoryUnit < ActiveRecord::Migration[5.2]
+class AddQlRMAReceivedDateToSpreeInventoryUnit < ActiveRecord::Migration[5.2]
   def change
     add_column :spree_inventory_units, :ql_rma_received, :datetime
   end
